3fe0892d41dcccd3f90602220c923b33933d204b
Kosolov325 <xxlucas325xx@gmail.com>
2022-09-11 16:26:29 -0300

    MEGA BRUH FIXING THE CODE AND FINAL STAGE

--- src/projeto1/projeto3.java
+++ src/projeto1/projeto3.java
@@ -11,62 +11,50 @@ public class projeto3 {
 	public static void main(String[] args) {
 		// 
 		Scanner keyboard = new Scanner(System.in);
-		System.out.printf("Digite a lista de números: ");
-		String nums = keyboard.nextLine();
+//		System.out.printf("Digite a lista de números: ");
+//		String nums = keyboard.nextLine();
+		String nums = "51 98 12 31 09 21 20";
 		Pattern p = Pattern.compile("\\d+");
 		Matcher m = p.matcher(nums);
-		ArrayList<Object> allgroups = new ArrayList<>();
-		ArrayList<Integer> tempgroup = new ArrayList<>();
+		ArrayList<Object> allGroups = new ArrayList<>();
+		ArrayList<Integer> tempGroup = new ArrayList<>();
 		ArrayList<Integer> lowestNumbers = new ArrayList<>();
-		ArrayList<Integer> finalGroup = new ArrayList<>();
 		int tempInteger;
 		int size = 1;
-		int tempsize = 1;
 		int lowestNumbersSize = 0;
-		int resultsSize = 0;
-		int finalElement = 0;
-	    String globalString = "";
 	    String piramide = "";
+	    
 	    while(m.find()){
-	    	globalString = globalString + m.group() + " ";
-	    	finalElement = Integer.parseInt(m.group());
-	    	resultsSize++;
-	    }
-	    finalGroup.add(finalElement);
-	    m.reset();
-	    while (m.find()) {
 	    	tempInteger = Integer.parseInt(m.group());
-	    	tempgroup.add(tempInteger);
-	    	if (tempsize == size) {
-	    		piramide = piramide + tempgroup + "\n";
-	    		ArrayList<Integer> nextgroup = (ArrayList)tempgroup.clone();
-	    		tempgroup.sort(Comparator.naturalOrder());
-	    		lowestNumbers.add(tempgroup.get(0));
-	    		lowestNumbersSize += tempgroup.get(0);
-	    		tempgroup.clear();
-	    		allgroups.add(nextgroup);
+	    	tempGroup.add(tempInteger);
+	    	if (tempGroup.size() == size) {
+	    		ArrayList<Integer> nextGroup = (ArrayList)tempGroup.clone();
+	    		tempGroup.sort(Comparator.naturalOrder());
+	    		lowestNumbers.add(tempGroup.get(0));
+	    		lowestNumbersSize += tempGroup.get(0);
+	    		tempGroup.clear();
+	    		allGroups.add(nextGroup);
+	    		piramide += nextGroup + "\n";
 	    		size++;
-	    		tempsize = 0;
 	    	}
-	    	tempsize++;
+	    	
 	    }
-	   if (resultsSize % 1 == 0 || resultsSize % resultsSize == 0){
-		   allgroups.add(finalGroup);
-		   lowestNumbers.add(finalElement);
-		   lowestNumbersSize += finalElement;
-		   piramide = piramide + finalGroup+ "\n";
-	   }
+	   piramide += tempGroup + "\n";
+	   tempGroup.sort(Comparator.naturalOrder());
+	   lowestNumbers.add(tempGroup.get(0));
+	   lowestNumbersSize += tempGroup.get(0);
+
 	   System.out.printf("\n");
-	   System.out.printf("Quantidade: " + resultsSize + "\n");
-	   System.out.printf("String: " + globalString + "\n");
-	   System.out.printf("Lista: " + allgroups);
+	   System.out.printf("Quantidade: " + size + "\n");
+	   System.out.printf("String: " + nums + "\n");
+	   System.out.printf("Lista: " + allGroups + tempGroup);
 	   System.out.printf("\n");
 	   System.out.printf("\n");
 	   System.out.printf("-----------------------\n");
 	   System.out.printf("-- PIRAMIDE --\n");
 	   System.out.printf(piramide);
 	   System.out.printf("-----------------------\n");
-	   System.out.println(allgroups);
+	   System.out.println(allGroups);
 	   System.out.printf("Os menores valores são: " + lowestNumbers + "\n");
 	   System.out.printf("A soma dos menores valores é: " + lowestNumbersSize);
 	 
